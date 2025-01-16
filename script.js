dc7a99487cf068275e92d2478b035b554e138cb6
nomaakip! <128547338+Nomaakip@users.noreply.github.com>
2025-01-16 21:04:55 +0100

    d

---
+++ script.js
@@ -0,0 +1,19 @@
+let clicks = JSON.parse(localStorage.getItem('clicks'));
+const clicksdisplay = document.getElementById('clicksh1');
+
+
+if (clicks) {
+    clicks = clicks;
+}
+
+else {
+    clicks = 0;
+}
+
+clicksdisplay.innerHTML = "Clicks: " + clicks;
+
+function clicked() {
+    clicks++;
+    clicksdisplay.innerHTML = "Clicks: " + clicks;
+    localStorage.setItem('clicks', JSON.stringify(clicks));
+}
