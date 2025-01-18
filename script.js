77ca48f232c1111b4d6622b689b525c93587370c
nomaakip! <128547338+Nomaakip@users.noreply.github.com>
2025-01-18 14:17:24 +0100

    yes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let gregg = localStorage.getItem('gregg');
 let clicks = JSON.parse(localStorage.getItem('clicks'));
 no.style.display = 'none';
 let switch2 = localStorage.getItem('switch2');
@@ -14,15 +15,32 @@ else {
 
 if (switch2) {
     switch2 = switch2;
+    y2div.style.display = 'flex';
 }
 
 else {
     switch2 = false;
 }
 
+if (gregg) {
+    gregg = gregg;
+    g2div.style.display = 'flex';
+    document.body.style.backgroundImage = "url('https://www.greg.com/images/resized_and_crop/250/200/eyJpZCI6IjgzYWU3NjQ2YjljYWZkYjBiYjAzY2MwY2U2Y2E3NmVmIiwic3RvcmFnZSI6InN0b3JlIn0?signature=87f4495c39748e4459b4f3ad69f5843a9acc005e7b3baa33ccad7d6e47f198dd')";
+    document.getElementById("clicksh1").style.color = '#ffff';
+    document.getElementById("upgrades").style.color = '#ffff';
+}
+
+else {
+    gregg = false;
+}
+
 clicksdisplay.innerHTML = "Clicks: " + clicks;
 
 function clicked() {
+    if (gregg) {
+        clicks += 9;
+    }
+
     if (switch2) {
         clicks += 3;
     }
@@ -44,6 +62,7 @@ function switch22() {
             switch2 = true;
             clicks -= 300;
             no.innerHTML = 'Successfully bought!';
+            y2div.style.display = 'flex';
         }
     }
 
@@ -58,3 +77,33 @@ function switch22() {
 function display() {
     upgradesdiv.style.display = 'flex';
 }
+
+function gregbuy() {
+    no.style.display = 'block';
+    if (!gregg) {
+        if (clicks < 1500) {
+            document.getElementById('no').innerHTML = 'not enough clicks!';
+        }
+        else {
+            gregg = true;
+            clicks -= 1500;
+            no.innerHTML = 'Successfully bought!';
+            g2div.style.display = 'flex';
+    document.body.style.backgroundImage = "url('https://www.greg.com/images/resized_and_crop/250/200/eyJpZCI6IjgzYWU3NjQ2YjljYWZkYjBiYjAzY2MwY2U2Y2E3NmVmIiwic3RvcmFnZSI6InN0b3JlIn0?signature=87f4495c39748e4459b4f3ad69f5843a9acc005e7b3baa33ccad7d6e47f198dd')";
+    document.getElementById("clicksh1").style.color = '#ffff';
+    document.getElementById("upgrades").style.color = '#ffff';
+        }
+    }
+
+    else {
+        no.innerHTML = 'already bought!';
+    }
+    clicksdisplay.innerHTML = "Clicks: " + clicks;
+    localStorage.setItem('clicks', JSON.stringify(clicks));
+    localStorage.setItem('gregg', gregg);
+}
+
+function display() {
+    upgradesdiv.style.display = 'flex';
+}
+
